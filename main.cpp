cfeb76007e11cba22e71b27a1814ed36aed2f090
Tomasz Zupnik <tzupnik7@gmail.com>
2014-01-22 17:10:12 +0100

    final main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+#include<iostream>
+#include"utils.h"
+
 int main(){
-	
+	int* tab=random();
+	print(tab);
+	std::cout<<"Minimum: "<<min(tab)<<std::endl;
+	std::cout<<"Maximum: "<<max(tab)<<std::endl;
 	return 0;
 }
 
